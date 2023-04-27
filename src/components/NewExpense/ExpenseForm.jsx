7fc7eee428d1abe058d2f799d8cdd378974928da
ZLN <zawlinnaing52@gmail.com>
2023-04-27 16:29:54 +0630

    Create ExpenseForm

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,6 +1,24 @@
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  return;
+  return (
+    <div>
+      <div className="new-expense__controls">
+        <label htmlFor="">Title </label>
+        <input type="text" name="" id="" />
+      </div>
+      <div className="new-expense__controls">
+        <label htmlFor="">Amount</label>
+        <input type="number" min="0.01" step="0.01" />
+      </div>
+      <div className="new-expense__controls">
+        <label htmlFor="">Date</label>
+        <input type="number" min="2019-01-01" max="2023-12-31" step="0.01" />
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add This Expense</button>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseForm;
